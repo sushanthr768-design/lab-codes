8ea7fc8cfd8afe2b47d924a14c60001e4c192d4e
sushanthr768-design <sushanthr768@gmail.com>
2026-07-23 14:46:32 +0530

    Add files via upload

---
+++ lab-2.py
@@ -0,0 +1,48 @@
+'''
+number=int(input("enter a number:"))
+#check if the number is greater than 0
+if number > 0:
+    print(f'{number} is a positive number.')
+    print ('a statement outside the if statement')
+    if number < 0:
+        print(f'{number} is a negative number .')
+        print('a stanement inside the if stanement')
+'''
+'''
+x=1
+total=0
+#start of the if stanement
+if x  != 0:
+    total += x
+    print(total)
+    #end of the if statement
+
+    print('this is always exicuted.')
+'''
+'''
+# Error code
+x = 1
+total = 0
+if x != 0:
+    total += x
+    print(total)
+'''
+'''
+number = int(input('Enter a number: '))
+
+if number > 0:
+    print('Positive number')
+else:
+    print('Not a positive number')
+
+print('This statement always executes')
+'''
+number = -5
+if  number > 0:
+    print('positive number')
+elif number < 0 :
+        print('negative number')
+else:
+    print('this statement is always exicuted')
+
+    
---
+++ phython.py
@@ -0,0 +1,11 @@
+# create a sequence from 2 to 4 (5 is not included)
+numbers = range(2, 5)
+print(list(numbers))    # [2, 3, 4]
+
+# create a sequence from -2 to 3
+numbers = range(-2, 4)    
+print(list(numbers))    # [-2, -1, 0, 1, 2, 3]
+
+# creates an empty sequence
+numbers = range(4, 2)
+print(list(numbers))    # []
